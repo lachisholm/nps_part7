8a1209c43f16375e5a9f42e3f97455ba5dcc1725
Lora Chisholm <chi20006@byui.edu>
2025-11-28 20:28:20 -0700

    build out and add import and add api key

---
+++ parkService.mjs
@@ -0,0 +1,18 @@
+import { getParkVisitorCenterDetails } from "./parkService.mjs";
+
+
+
+// generic JSON fetch helper
+async function getJson(endpoint) {
+    const baseUrl = "https://developer.nps.gov/api/v1/";
+    const apiKey = "Y4ehKlRiC48UV0vFbh5ygd97irtGn5fhdvhsCHO1P"; 
+    const response = await fetch(`${baseUrl}${endpoint}&api_key=${apiKey}`);
+    const data = await response.json();
+    return data;
+}
+
+// get details for ONE visitor center
+export async function getParkVisitorCenterDetails(id) {
+    const result = await getJson(`visitorcenters?id=${id}`);
+    return result.data[0];
+}
